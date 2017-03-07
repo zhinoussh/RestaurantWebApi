cae6e9cbdbe9f9bb718c133055bd41f713440f2c
zhinoussh <spring1986@gmail.com>
2017-03-07 16:06:18 +0330

    add tbl_user_profile and set relations to ASPNetUser table

--- WebAPI_for_Anugular_Restaurant/DataAccessLayer/RestaurantDBContext.cs
+++ WebAPI_for_Anugular_Restaurant/DataAccessLayer/RestaurantDBContext.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Data.Entity;
 using WebAPI_for_Anugular_Restaurant.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace WebAPI_for_Anugular_Restaurant.DataAccessLayer
 {
@@ -11,7 +12,7 @@ namespace WebAPI_for_Anugular_Restaurant.DataAccessLayer
     {
         public virtual DbSet<tbl_Meals_Category> tbl_Meal_Category { get; set; }
         public virtual DbSet<tbl_Sub_Category> tbl_Sub_Category { get; set; }
-
+        public virtual DbSet<tbl_user_profile> tbl_user_profile { get; set; }
 
         public RestaurantDBContext()
             : base("RestaurantConnection")
@@ -26,6 +27,15 @@ namespace WebAPI_for_Anugular_Restaurant.DataAccessLayer
                 .WithOptional(e => e.Category)
                 .HasForeignKey(e => e.CategoryID)
                 .WillCascadeOnDelete();
+
+            modelBuilder.Entity<tbl_user_profile>()
+              .HasRequired(a => a.ApplicationUser)
+              .WithMany()
+              .HasForeignKey(u => u.UserID);
+
+            modelBuilder.Entity<IdentityUserLogin>().HasKey<string>(l => l.UserId);
+            modelBuilder.Entity<IdentityRole>().HasKey<string>(r => r.Id);
+            modelBuilder.Entity<IdentityUserRole>().HasKey(r => new { r.RoleId, r.UserId });
                 
         }
     }
---
+++ WebAPI_for_Anugular_Restaurant/Models/Entities/tbl_user_profile.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace WebAPI_for_Anugular_Restaurant.Models
+{
+    public class tbl_user_profile
+    {
+        [Key]
+        public int ProfileID { get; set; }
+
+        public String PhoneNumber { get; set; }
+
+        public String Address { get; set; }
+
+        [ForeignKey("ApplicationUser")]
+        public string UserID { get; set; }
+
+        public ApplicationUser ApplicationUser { get; set; }
+    }
+}
